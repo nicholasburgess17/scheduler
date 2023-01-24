296f177fcfaaa3d67b6317fb80b0c523bb6d8b9f
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-01-23 23:16:47 -0800

    import scss to daylist component, use classNames library to determind styling classes

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ export default function Button(props) {
     "button--confirm": props.confirm,
     "button--danger": props.danger,
   });
-
+  
   return (
     <button
       className={buttonClass}
--- src/components/dayListItem.js
+++ src/components/dayListItem.js
@@ -1,11 +1,18 @@
 import React from "react";
+import "components/dayListItem.scss"
+import classNames from "classnames";
 
 export default function DayListItem(props) {
-  
+  let dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots
+
+  })
   return (
     <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{props.spots} spots remaining</h3>
     </li>
   );
 }

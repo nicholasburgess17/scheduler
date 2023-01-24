df94aa866dd8f99e53db452d7e2c2316d8f954b1
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-01-23 22:45:00 -0800

    create dayListItem component to display days of the week

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "index.scss";
-
+import DayListItem from "components/dayListItem";
 import Application from "components/Application";
+
 
 ReactDOM.render(<Application />, document.getElementById("root"));

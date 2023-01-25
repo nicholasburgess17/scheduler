7eadba5e0b178c213e2219ebf1bc0e99ec89c48b
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-01-24 22:27:36 -0800

    set up Daylist component, add tests for dayListItem and add functionality for 0 and 1 spots in daylistitems

--- src/components/dayListItem.js
+++ src/components/dayListItem.js
@@ -1,17 +1,25 @@
 import React from "react";
-import "components/dayListItem.scss"
+import "components/dayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
-  let dayClass = classNames("day-list__item",{
+  let dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0,
-
-  })
+  });
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    }
+    return `${props.spots} spots remaining`;
+  };
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li onClick={() => props.setDay(props.name)} selected={props.selected}>
       <h2 className={dayClass}>{props.name}</h2>
-      <h3 className={dayClass}>{props.spots} spots remaining</h3>
+      <h3 className={dayClass}>{formatSpots()}</h3>
     </li>
   );
 }

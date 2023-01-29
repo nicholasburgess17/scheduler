2d307efe0ac0e9f4bae3a0929049944103d87c5a
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-01-29 14:07:09 -0800

    uncomment appointment booked story, add appointment empty logic

--- src/components/appointments/index.js
+++ src/components/appointments/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "components/appointments/styles.scss";
+import Header from "./header";
 // import Confirm from "./confirm";
-// import Empty from "./empty";
-// import Header from "./header";
-// import Show from "./show";
+import Empty from "./empty";
+import Show from "./show";
 // import Status from "./status";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 }

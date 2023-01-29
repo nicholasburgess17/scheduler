e96baee4b998c340bf2a06019e7536f8eb093b39
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-01-29 14:53:56 -0800

    create appointment components function, apply function to HTML

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import "components/Application.scss";
 import DayList from "./dayList";
+import Appointment from "./appointments";
 
 const days = [
   {
@@ -20,9 +21,54 @@ const days = [
     spots: 0,
   },
 ];
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const appointmentComps = Object.values(appointments).map(appointments => {
+    return (
+      <Appointment
+        key={appointments.id}
+        {...appointments}
+      />);
+  });
   return (
     <main className="layout">
       <section className="sidebar">
@@ -41,7 +87,7 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule"></section>
+      <section className="schedule">{appointmentComps}</section>
     </main>
   );
 }

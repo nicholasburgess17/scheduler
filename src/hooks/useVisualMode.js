b3ff62709a382338d4778ba98c15503a2bdfba83
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-02-04 16:27:05 -0800

    remove console.logs, logging states

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,7 +5,6 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
   const newHistory = [...history];
 
-
   function transition(newMode, replace = false) {
     console.log(`in transition fn, newMode = ${newMode}, replace = ${replace}`);
     //replace mode with new mode
@@ -14,19 +13,15 @@ export default function useVisualMode(initial) {
     }
     //add new mode to stack
     setMode(newMode);
-    // console.log(newMode);
-    // console.log(...newHistory);
     setHistory([...newHistory, newMode]);
-    // setHistory(newHistory);
-    // console.log(newHistory);
   }
+
   function back() {
     console.log("going back");
     //avoid initial mode
     if (history.length === 1) {
       return;
     }
-
     //set new mode, update history state
     newHistory.pop();
     setMode(newHistory[newHistory.length - 1]);

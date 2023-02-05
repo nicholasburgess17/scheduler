5de4c8db06d2bb1db5e36a37f3890198b36d6de3
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-02-04 17:15:15 -0800

    add Create functionality for appointments, add cancel functionality using back function from useVisualMode

--- src/components/appointments/index.js
+++ src/components/appointments/index.js
@@ -8,6 +8,7 @@ import Header from "./header";
 import Empty from "./empty";
 import Show from "./show";
 // import Status from "./status";
+import Form from "./form";
 
 //hooks
 import useVisualMode from "hooks/useVisualMode";
@@ -15,6 +16,7 @@ import useVisualMode from "hooks/useVisualMode";
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE"
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -23,7 +25,7 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
 
       {mode === SHOW && (
         <Show
@@ -31,6 +33,12 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         />
       )}
+      {mode === CREATE && <Form
+          name={props.name}
+          value={props.value}
+          interviewers={[]}
+          onCancel={back}
+        /> }
     </article>
   );
 }

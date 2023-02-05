c10ea8ff3807a03afc5e50d9ed7e2e2daa029268
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-02-04 16:25:49 -0800

    add back function in useVisualMode state, refactor transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,22 +2,40 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  const newHistory = [...history];
 
-  function transition(transtitionMode, replace = false) {
-    const newMode = [...mode];
 
+  function transition(newMode, replace = false) {
+    console.log(`in transition fn, newMode = ${newMode}, replace = ${replace}`);
+    //replace mode with new mode
     if (replace) {
-      newMode.pop();
-    };
-    setMode(transtitionMode);
-  };
+      newHistory.pop();
+    }
+    //add new mode to stack
+    setMode(newMode);
+    // console.log(newMode);
+    // console.log(...newHistory);
+    setHistory([...newHistory, newMode]);
+    // setHistory(newHistory);
+    // console.log(newHistory);
+  }
   function back() {
+    console.log("going back");
+    //avoid initial mode
+    if (history.length === 1) {
+      return;
+    }
 
-  };
+    //set new mode, update history state
+    newHistory.pop();
+    setMode(newHistory[newHistory.length - 1]);
+    setHistory(newHistory);
+  }
 
   return {
     mode,
     transition,
-    back
+    back,
   };
 }

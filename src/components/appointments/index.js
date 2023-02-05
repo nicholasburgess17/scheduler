7b5980fc3715410c810af17008ceef9e8d4ab2b6
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-02-04 20:36:18 -0800

    add comments

--- src/components/appointments/index.js
+++ src/components/appointments/index.js
@@ -19,20 +19,23 @@ export default function Appointment(props) {
   const CREATE = "CREATE"
 
   const { mode, transition, back } = useVisualMode(
+    //if there is an interview, show it, else show empty
     props.interview ? SHOW : EMPTY
   );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
+      {/* if empty, allow usage of create component */}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-
+      {/* if there is an interview, use show component */}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
       )}
+      {/* if mode is create, show form, allwo canelation using back function in useVisualMode */}
       {mode === CREATE && <Form
           name={props.name}
           value={props.value}

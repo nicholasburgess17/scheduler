03c4f3d86a93c74edc7c360fdc96670326c0011c
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-02-05 16:35:03 -0800

    adjust data so all tests pass

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,19 +1,19 @@
-export  function getAppointmentsForDay(state, day) {
-  //find day that matches, returns object 
-  const matchDay = state.days.find((days) => days.name === day);
 
-  //if not matching, return an empty array
-  if (!matchDay) {
-    return [];
-  }
+export function getAppointmentsForDay(state, day) {
+  // returns object matching day
+  const matchingDay = state.days.find((days) => days.name === day);
 
-  // returns an array of appointment ids for day
-  const appointmentIDs = matchDay.appointments;
+  // if day is not found, return empty object
+  if (!matchingDay) return [];
 
-  // returns an array wehere appointmentIDs is replaced with the correct appointment details
-  const appointmentList = appointmentIDs.map((id) => state.appointments[id]);
-  return appointmentList;
+  // returns array of appointment ids for day
+  const appointmentIds = matchingDay.appointments;
+
+  // returns a new array where each appointmentId is replaced with it's corresponding appointment details
+  const appointmentsList = appointmentIds.map((id) => state.appointments[id]);
+  return appointmentsList;
 }
+
 
 //appointment object structure post transformation via application component
 // {  
@@ -39,4 +39,41 @@ export function getInterview(state, interview) {
       interviewer: state.interviewers[id],
     };
   }
+}
+
+export function getInterviewersForDay(state, day) {
+  // if days data is empty return empty array
+  if (state.days.length === 0) {
+    return [];
+  }
+
+  // Filter state.days array to find the object who's name matches the provided day
+  const filteredDays = state.days.filter((selectedDay) => {
+    return day === selectedDay.name;
+  });
+
+  //  return empty array if day not found
+  if (filteredDays.length === 0) {
+    return [];
+  }
+
+  // array of interviewers from filteredDays object
+  const interviewerIds = filteredDays[0].interviewers;
+
+  // if no interviewers on the given day, return empty array
+  if (interviewerIds.length === 0) {
+    return [];
+  }
+
+  // create variable for resulting interviewers data
+  let selectedDayInterviewers = [];
+
+  // iterate through the interviewer array for the given day
+  for (const id of interviewerIds) {
+    // find interviewers that match the id from apptsFromObj and push into result
+    const interviewer = state.interviewers[id];
+    selectedDayInterviewers.push(interviewer);
+  }
+  // return an array of interviewers for the selected day
+  return selectedDayInterviewers;
 }

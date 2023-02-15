74d98548948d7a691f55fa098f1698779cd1bde3
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-02-15 14:03:25 -0800

    refactor InterviewerList function to accept propTypes to ensure correct data is passed

--- src/components/interviewerList.js
+++ src/components/interviewerList.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "components/interviewerList.scss";
 import InterviewerListItem from "./interviewerListItem";
+import PropTypes from 'prop-types';
 
-// const interviewers = [
-//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-// ];
-export default function InterviewerList(props) {
+
+
+function InterviewerList(props) {
   const interviewerList = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
@@ -28,3 +24,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
